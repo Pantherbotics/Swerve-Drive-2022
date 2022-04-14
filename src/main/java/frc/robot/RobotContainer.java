50e6fa61ebd5c15d45a64811289d55626bdb631d
Pantherbotics - Club Account <pantherbotics@gmail.com>
2022-04-14 16:24:22 -0700

    Broken but saving because i'm changing stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,10 +11,10 @@ import frc.robot.subsystems.Drivetrain;
 @SuppressWarnings("unused")
 public class RobotContainer {
     //Subsystems
-    private final Drivetrain drivetrain = new Drivetrain();
+    private final Drivetrain drivetrain = new Drivetrain(this);
 
     //Joysticks
-    private final XboxController pJoy = new XboxController(Constants.pJoyID);
+    public final XboxController pJoy = new XboxController(Constants.pJoyID);
     private final JoystickButton joyBA = new JoystickButton(pJoy, 1);
     private final JoystickButton joyBB = new JoystickButton(pJoy, 2);
     private final JoystickButton joyBX = new JoystickButton(pJoy, 3);
@@ -32,7 +32,7 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         drivetrain.setDefaultCommand(new RunCommand(() ->
-                drivetrain.runSwerve(pJoy.getRawAxis(0), pJoy.getRawAxis(1), pJoy.getRawAxis(4), pJoy.getRawAxis(5))
+                drivetrain.runSwerve(pJoy.getRawAxis(0), pJoy.getRawAxis(1), pJoy.getRawAxis(4), pJoy.getRawAxis(5)), drivetrain
         ));
 
         joyBA.whenPressed(new RunDriveMode(drivetrain, 1));
@@ -43,6 +43,8 @@ public class RobotContainer {
 
     public void updateSmartDashboard() {
         SmartDashboard.putNumber("Mode", drivetrain.mode);
+        SmartDashboard.putNumber("Gyro", drivetrain.getGyroRot());
+        SmartDashboard.putNumber("Gyro Target", drivetrain.targetGyro);
     }
 
     public double powAxis(double a, double b) {
@@ -53,7 +55,9 @@ public class RobotContainer {
         }
     }
 
-    public void init() {}
+    public double gyroTarget = 0;
+    public void init() {
+    }
 
     public void whenDisabled() {}
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,7 +1,16 @@
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 import frc.robot.util.Wheel;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @SuppressWarnings("unused")
 public class Drivetrain extends SubsystemBase {
@@ -9,20 +18,54 @@ public class Drivetrain extends SubsystemBase {
     private final Wheel RightFront;
     private final Wheel LeftRear;
     private final Wheel RightRear;
-    public int mode = 1;
+    private final AHRS gyro = new AHRS(I2C.Port.kOnboard);
+    public int mode = 4;
+
 
     //steering 
     double L = 18; //length
     double W = 18; // Width
     double R = Math.sqrt(L*L+W*W);
 
-    public Drivetrain(){
-        //Steering
-        LeftFront  = new Wheel(1, -80);
-        RightFront = new Wheel(2, 160);
+    public double targetGyro = 0;
+    public double initialGyro = 0;
+    public Drivetrain(RobotContainer robotContainer) {
+        //Steering (Positive is Clockwise Looking at bottom of robot)
+        LeftFront  = new Wheel(1, -75);
+        RightFront = new Wheel(2, 165);
         RightRear  = new Wheel(3,   0);
-        LeftRear   = new Wheel(4,-170);
+        LeftRear   = new Wheel(4,-150);
+
+        zeroGyro();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                setGyroTarget();
+                initialGyro = getGyroRot() % 360;
+            }
+        }, 500);
+
+        AtomicBoolean previousTrue = new AtomicBoolean(false);
+        Notifier notifier = new Notifier(() -> {
+            double steerAxis = robotContainer.pJoy.getRawAxis(4);
+            SmartDashboard.putNumber("SteerAxis", steerAxis);
+            if (Math.abs(steerAxis) >= 0.05) {
+                targetGyro += steerAxis*2D;
+                previousTrue.set(true);
+            }else if (previousTrue.get()) {
+                //targetGyro = getGyroRot();
+                previousTrue.set(false);
+            }
+        });
+        notifier.startPeriodic(20/1000D);
     }
+
+    public void zeroGyro() { gyro.reset(); }
+    public void setGyroTarget() { targetGyro = getGyroRot() % 360; }
+    public double getGyroRot() {
+        return gyro.getAngle() % 360;
+    }
+
 
     public void setMode(int mode) {
         this.mode = mode;
@@ -87,49 +130,54 @@ public class Drivetrain extends SubsystemBase {
 
         //Full Swerve Mode Right stick strafes left stick rotates.
         if (mode == 4) {
-            //define the rotation vector.
-            double rotX = XL/1.41; //R/sqrt2
-            double rotY = XL/1.41;
+            XL = -XL;
+            //Debounce
+            if (Math.abs(XL) <= 0.05) { XL = 0; }
+            if (Math.abs(YL) <= 0.05) { YL = 0; }
+
+            double gyroErrorDeg = targetGyro - getGyroRot();
+            double offset = initialGyro - getGyroRot(); //For Field-Oriented Swerve
+            double rotX = gyroErrorDeg/50 + offset/100;//gyroErrorDeg/60;
+            double rotY = gyroErrorDeg/50 + offset/100;//gyroErrorDeg/60;
+
+            SmartDashboard.putNumber("GyroInit", initialGyro);
+            SmartDashboard.putNumber("Offset", offset);
 
             //calculate base vectors
-            double X1 = XR+rotX;//left Front
-            double Y1 = YR+rotY;
+            double X1 = XL+rotX;//left Front
+            double Y1 = YL+rotY;
             double tAng1 = -Math.atan2(Y1,X1)*180/Math.PI+90;
-            double X2 = XR+rotX;//Right Front
-            double Y2 = YR-rotY;
+            double X2 = XL+rotX;//Right Front
+            double Y2 = YL-rotY;
             double tAng2 = -Math.atan2(Y2,X2)*180/Math.PI+90;
-            double X3 = XR-rotX;//Right Rear
-            double Y3 = YR-rotY;
+            double X3 = XL-rotX;//Right Rear
+            double Y3 = YL-rotY;
             double tAng3 = -Math.atan2(Y3,X3)*180/Math.PI+90;
-            double X4 = XR-rotX;//Left Rear
-            double Y4 = YR+rotX;
+            double X4 = XL-rotX;//Left Rear
+            double Y4 = YL+rotX;
             double tAng4 = -Math.atan2(Y4,X4)*180/Math.PI+90;
-            //Calculate speeds
-            double s1 = Math.sqrt(X1*X1+Y1*Y1);
-            double s2 = Math.sqrt(X2*X2+Y2*Y2);
-            double s3 = Math.sqrt(X3*X3+Y3*Y3);
-            double s4 = Math.sqrt(X4*X4+Y4*Y4);
+            //Calculate speeds (Divide by sqrt2 so that the speed of x1 y1 is also 1
+            double s1 = Math.sqrt(X1*X1+Y1*Y1) / 1.41;
+            double s2 = Math.sqrt(X2*X2+Y2*Y2) / 1.41;
+            double s3 = Math.sqrt(X3*X3+Y3*Y3) / 1.41;
+            double s4 = Math.sqrt(X4*X4+Y4*Y4) / 1.41;
             //Check to see if max speed is <1
-            double sMax = Math.max(s1,s2);
-            sMax = Math.max(sMax,s3);
-            sMax = Math.max(sMax,s4);
+            double sMax = Math.max(Math.max(Math.max(s1,s2), s3), s4);
 
-            if(sMax > 1) {
+            if (sMax > 1) {
                 s1 = s1/sMax;
                 s2 = s2/sMax;
                 s3 = s3/sMax;
                 s4 = s4/sMax;
-            } else{ //rescale to the square of the largest speed.
-                s1 = s1*sMax;
-                s2 = s2*sMax;
-                s3 = s3*sMax;
-                s4 = s4*sMax;
             }
             //invoke Wheel
             LeftFront.setState(s1, tAng1);
             RightFront.setState(s2,tAng2);
             RightRear.setState(s3, tAng3);
             LeftRear.setState(s4, tAng4);
+
+            //Remove the rotation vector's speed influence
+            // Aka have it affect rotation but not speed
         }
     }
 }
